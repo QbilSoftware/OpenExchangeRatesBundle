28b243f73579a4bedd9f0cf115555d225a98fea8
Faizan Akram <faizanakram99@gmail.com>
2018-06-03 16:42:38 +0530

    v1.1.0 + Added more methods + Refactored a bit

--- Domain/Exchange.php
+++ Domain/Exchange.php
@@ -14,68 +14,141 @@ use GuzzleHttp\Exception\GuzzleException;
 
 /**
  * Class Exchange
- * @method array latest(array $args = []) Get latest foreign rates from openexhangerates.com
- * @method array historical(array $args = []) Get foreign rates of a particular date from openexhangerates.com
  */
 class Exchange
 {
+    /**
+     * @var
+     */
     private $appId;
 
     /** @var Client */
     private $client;
 
+    /**
+     * @var array
+     */
     private $options = [];
 
+    /**
+     * Exchange constructor.
+     * @param $appId
+     * @param null $baseCurrency
+     */
     public function __construct($appId, $baseCurrency = null)
     {
+        $this->options = [];
         $this->appId = $appId;
         $this->client = new Client([
             'base_uri' => 'https://openexchangerates.org/api',
         ]);
 
-        $this->options = [
-            'query' => [
-                'app_id' => $this->appId,
-            ]
-        ];
+        $this->options['app_id'] = $this->appId;
 
         if ($baseCurrency) {
-            $this->options['query']['base'] = $baseCurrency;
+            $this->options['base'] = $baseCurrency;
         }
     }
 
-    public function __call($name, $arguments)
+    /**
+     * @param array $args
+     * @return array
+     * @throws GuzzleException
+     */
+    public function latest(array $args = [])
     {
-        if (!in_array($name, ["latest", "historical"])) {
-            throw new \BadMethodCallException("Call to undefined method $name. Valid methods are `latest` and `historical`");
+        return $this->fetch('/latest.json', ['query' => array_merge($args, $this->options)]);
+    }
+
+
+    /**
+     * @param array $args
+     * @return array
+     * @throws GuzzleException
+     */
+    public function historical(array $args)
+    {
+        if (!$args['date']) {
+            throw new \InvalidArgumentException('Required parameter `date` is missing');
         }
 
-        $path = "/$name";
+        return $this->fetch("/historical/{$args['date']}.json", ['query' => array_merge($args, $this->options)]);
+    }
 
-        if ($arguments[0]['symbols'] || $arguments[0]['currency']) {
-            $this->options['query']['symbols'] = $arguments[0]['symbols'] ?: $arguments[0]['currency'];
+    /**
+     * @param array $args
+     * @return array
+     * @throws GuzzleException
+     */
+    public function between(array $args)
+    {
+        if (!$args['start']) {
+            throw new \InvalidArgumentException('Required parameter `start` is missing');
         }
 
-        if ($arguments[0]['base']) {
-            $this->options['query']['base'] = $arguments[0]['base'];
+        if (!$args['end']) {
+            throw new \InvalidArgumentException('Required parameter `end` is missing');
         }
 
-        if ("historical" === $name) {
-            if (!$arguments[0]['date']) {
-                throw new \InvalidArgumentException('Required parameter `date` is missing');
-            }
+        return $this->fetch("/time-series.json", ['query' => array_merge($args, $this->options)]);
+    }
 
-            $path .= "/{$arguments[0]['date']}";
+    /**
+     * @param array $args
+     * @return string
+     * @throws GuzzleException
+     */
+    public function convert(array $args)
+    {
+        if (!$args['amount']) {
+            throw new \InvalidArgumentException('Required parameter `amount` is missing');
         }
 
-        try {
-            $response = $this
+        if (!$args['from']) {
+            throw new \InvalidArgumentException('Required parameter `from` is missing');
+        }
+
+        if (!$args['to']) {
+            throw new \InvalidArgumentException('Required parameter `to` is missing');
+        }
+
+        return $this->fetch("/historical/{$args['amount']}/{$args['from']}/{$args['to']}.json", ['query' => array_merge($args, $this->options)], 'response');
+    }
+
+    /**
+     * @return array
+     * @throws GuzzleException
+     */
+    public function currencies()
+    {
+        return json_decode(
+            $this
                 ->client
-                ->request('GET', "$path.json", $this->options);
-        } catch (GuzzleException $e) {
-            return json_encode($e->getMessage());
-        }
+                ->request('GET', '/currencies.json')
+                ->getBody()
+                ->getContents(),
+            true
+        );
+    }
 
-        return json_decode($response->getBody()->getContents(), true)['rates'];
+    /**
+     * @param $route
+     * @param array $options
+     * @param $responseKey
+     * @return array|string
+     * @throws GuzzleException
+     */
+    private function fetch($route, array $options, $responseKey = 'rates')
+    {
+        $response = json_decode(
+            $this
+                ->client
+                ->request('GET', $route, $options)
+                ->getBody()
+                ->getContents(),
+            true
+        );
+
+        return $response[$responseKey];
     }
 }
--- README.md
+++ README.md
@@ -23,13 +23,27 @@ For example,
 
 In you controller you can get instance of `qbil_open_exchange_rate.exchange` service by `$this-get('qbil_open_exchange_rate.exchange')`
 
-The service provides two public methods `latest` and `historical` to get latest and historical (fx rate of a particular date) foreign exchange rates from openexchangerates.com. More methods will be added soon
 
-Both methods accept an associative array as argument with following keys
+**Methods**
 
-1) `symbols` or `currency` -  Get foreign exchange rates of only give currencies (in comma separated format). 
+The service provides five public methods `latest`, `historical`, `between`, `convert` and `currencies`.
+
+All methods except `currencies` (which accepts no argument) can accept an associative array as argument with following keys
+
+1) `symbols` or `currencies` -  Get foreign exchange rates of only give currencies (in comma separated format). 
 <br /> <br /> For example <br /> `$this-get('qbil_open_exchange_rate.exchange')->latest(['symbols' => 'USD,EUR,GBP'])` will return an array of latest foreign exchange rates of currencies USD, EUR and GBP
 2) `base` - Even if you specified base currency in `parameters.yml` file, you can override it in each request adding `base` key to arguments array
 <br /> <br /> For example <br /> `$this-get('qbil_open_exchange_rate.exchange')->latest(['base' => 'GBP'])`  will return an array of latest foreign exchange rates with respect to base currency GBP
 
-`historical` method has a required argument key `date` (yyyy-mm-dd format), i.e. the date of which foreign exchange rates you want to fetch .e.g. `$this-get('qbil_open_exchange_rate.exchange')->latest(['date' => '2017-10-01'])` will return an array of exchange rates of date 2017-10-01 .
+**Methods explained** 
+1) `latest` - This method fetches latest foreign exchange rates. e.g. `$this-get('qbil_open_exchange_rate.exchange')->latest()` will return an array of latest exchange rates.
+
+2) `historical` - This method fetches foreign exchange rates of a particular date. It has a required argument key `date` (yyyy-mm-dd format), i.e. the date of which foreign exchange rates you want to fetch .e.g. `$this-get('qbil_open_exchange_rate.exchange')->historical(['date' => '2017-10-01'])` will return an array of exchange rates of date 2017-10-01.
+
+3) `between` - This method fetches foreign exchange rates between particular dates specified by arguments key `start` and `end` (both in yyyy-mm-dd format) .e.g. `$this-get('qbil_open_exchange_rate.exchange')->between(['start' => '2017-10-01', 'end' => '2017-11-05'])` will return an array of exchange rates of between 2017-10-01 and 2017-11-05.
+
+4) `convert` - This method is used to convert any money value from one currency to another at the latest foreign exchange rates. It has three required argument keys `amount`, `from` and `to`. `from` and `to` are currency codes (3 letters) and `amount` is the amount you want to convert. e.g. `$this-get('qbil_open_exchange_rate.exchange')->between(['amount' => '15678800', 'from' => 'USD', 'to' => 'EUR'])` will return equivalent amount in EUR (as string)
+
+5) `currencies` - This method returns array of all supported currencies with symbol as their key and currency as their value.
+
+Note: Some of the above methods are only available in enterprise or ultimate edition. Please visit https://openexchangerates.com for more info
