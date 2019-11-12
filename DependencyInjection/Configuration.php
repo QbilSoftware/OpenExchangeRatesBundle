13a0ae95a6a3b2f8ad57aa11196e9c570a041da0
Junaid Farooq <ulislam.junaid125@gmail.com>
2019-11-12 19:47:07 +0530

    Handled deprecation related to tree builder to make it fully symfony 4 compatible

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('qbil_open_exchange_rate');
+        $treeBuilder = new TreeBuilder('qbil_open_exchange_rate');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
